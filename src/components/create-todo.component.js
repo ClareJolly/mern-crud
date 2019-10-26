b158a5abef43394892e87af0bd7afc0e8626649c
Clare Jolly <clarejollyuk@gmail.com>
2019-10-26 15:25:16 +0100

    hooking up front and backend

--- src/components/create-todo.component.js
+++ src/components/create-todo.component.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+
 export default class CreateTodo extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            todo_description: '',
-            todo_responsible: '',
-            todo_priority: '',
-            todo_completed: false
-        }
+        // this.state = {
+        //     todo_description: '',
+        //     todo_responsible: '',
+        //     todo_priority: '',
+        //     todo_completed: false
+        // }
+    }
+
+    state = {
+        todo_description: '',
+        todo_responsible: '',
+        todo_priority: '',
+        todo_completed: false
     }
 
     onChangeTodoDescription = (e) => {
@@ -26,14 +35,24 @@ export default class CreateTodo extends Component {
         });
     }
 
-    onSubmit= (e) => {
+    onSubmit = (e) => {
         e.preventDefault();
         
         console.log(`Form submitted:`);
         console.log(`Todo Description: ${this.state.todo_description}`);
         console.log(`Todo Responsible: ${this.state.todo_responsible}`);
         console.log(`Todo Priority: ${this.state.todo_priority}`);
-        
+     
+        const newTodo = {
+            todo_description: this.state.todo_description,
+            todo_responsible: this.state.todo_responsible,
+            todo_priority: this.state.todo_priority,
+            todo_completed: this.state.todo_completed
+        };
+
+        axios.post('http://localhost:4000/todos/add', newTodo)
+            .then(res => console.log(res.data));
+
         this.setState({
             todo_description: '',
             todo_responsible: '',
@@ -41,7 +60,11 @@ export default class CreateTodo extends Component {
             todo_completed: false
         })
     }
+
+
     render() {
+
+        console.log(this.state.todo_completed)
         return (
             <div style={{marginTop: 10}}>
                 <h3>Create New Todo</h3>
